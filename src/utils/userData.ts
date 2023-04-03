bb6409d7ca76affc14802a000f5eb6f42cb6a4b7
Josiel Luz <josiel9511791722@gmail.com>
2023-04-03 03:15:07 -0300

    new commit

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Josiel Luz",
+  githubUser: "Josiel-Luz",
+  linkedinUser: "josiel-luz",
+  whatsappNumber: "+98999749805",
+  emailUser: "josiel9511791722@kenzie.com.br",
+  instagramUser: "",
   facebookUser: "",
 };
